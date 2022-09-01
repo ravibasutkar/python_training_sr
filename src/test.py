1cd9ed639b3981c671e851851ae1a713c5c80364
Sb <ravikiran.sb@capgemini.com>
2022-09-01 11:07:22 +0530

    new commit22

--- src/test.py
+++ src/test.py
@@ -10,3 +10,13 @@ print(v3)
 m=10
 vformat=f'hello world {m}'   # or string.format
 vformat
+
+#time complexity of list of insertion or    or  deletion operation
+#O(1)
+
+#map reduce lambda
+#code snippet
+
+#tuple
+#List we cant do unpacking
+
